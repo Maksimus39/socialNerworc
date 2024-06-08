a91731994574d43fe7f14f03b83a7a324a451bf5
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-08 11:26:07 +0300

    Вынес стили компоненты и обернул на уровень выше.

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import classes from './Profile.module.css'
 import {MyPosts} from "./myPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
                 <img
                     src="https://wenet.pl/blog/wp-content/uploads/2021/02/Zmiany_na_Facebooku_dla_reklamodawcow_czyli_testowanie_wykluczen_tematow-e1624269470332.jpg"
