7b751612e25e3c2c31ba9b2616be3503d57519ef
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-10 12:27:22 +0300

    В этой компоненте я добавил классы

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
 import {MyPosts} from "./myPosts/MyPosts";
 
 export const Profile = () => {
     return (
-        <div>
-            <div>
+        <div className="profile-container">
+            <div className="profile-img-wrapper">
                 <img
+                    className="profile-img"
                     src="https://wenet.pl/blog/wp-content/uploads/2021/02/Zmiany_na_Facebooku_dla_reklamodawcow_czyli_testowanie_wykluczen_tematow-e1624269470332.jpg"
-                    alt=""/>
+                    alt="Профиль"/>
             </div>
             <div>
-                ava + discription
+                ava + description
             </div>
             <MyPosts/>
         </div>
     );
 };
-
