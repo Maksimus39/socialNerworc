f564d428b46de1e381fdf071657e3dcf2c89ab4e
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-10 12:14:52 +0300

    Добавил новые компоненты и обернул их роутингом

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,35 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navdar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import classes from "*.module.css";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
 
 function App() {
+
     return (
-        <div className="app-wrapper">
+        <BrowserRouter>
 
-            <Header/>
-            <Navbar/>
+            <div className='app-wrapper'>
 
-            <div className="app-wrapper-content">
-                <Dialogs/>
+                <Header/>
+                <Navbar/>
+
+                <div className='app-wrapper-content'>
+
+                    <Routes>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                    </Routes>
+
+                </div>
             </div>
-
-            {/*<Profile/>*/}
-
-        </div>
-    );
+        </BrowserRouter>
+    )
 }
 
 export default App;
