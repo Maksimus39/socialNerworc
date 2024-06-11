cf0498e7b04a807583faf7ce4980dfe4c5736160
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-12 01:07:32 +0300

    Проимпортировал модульные классы в компоненту Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,30 @@
+import classes from './Dialogs.module.css'
+
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogItems}>
+                <div className={classes.dialog}>
+                    Max
+                </div>
+                <div className={classes.dialog}>
+                    Larisa
+                </div>
+                <div className={classes.dialog}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Bogdan
+                </div>
+            </div>
+
+            <div className={classes.messages}>
+                <div className={classes.message}>Привет, семья! Как ваши дела сегодня?</div>
+                <div className={classes.message}>Здравствуй, дорогой! Всё хорошо, занимаемся подготовкой к ужину.</div>
+                <div className={classes.message}>Привет, всем! Я только что вернулся с тренировки, было классно.</div>
+                <div className={classes.message}>Привет, пап! Я заканчиваю домашнее задание, скоро буду свободен.</div>
+            </div>
+
         </div>
     )
 }
