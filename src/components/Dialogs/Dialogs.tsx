70e338e878b0b7cbb6a4dc95ddda720e8295e319
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-15 15:12:18 +0300

    Рефакторинг данных в компоненте.

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -19,6 +19,7 @@ export const DialogsItem = (props: DialogsPropsItem) => {
 
 type PropsMessage = {
     message: string
+    id: number
 }
 
 export const Message = (props: PropsMessage) => {
@@ -27,22 +28,39 @@ export const Message = (props: PropsMessage) => {
 
 
 export const Dialogs = () => {
+
+
+    let dialogsData = [
+        {name: "Max", id: 1},
+        {name: "Larisa", id: 2},
+        {name: "Andrey", id: 3},
+        {name: "Bogdan", id: 4}
+    ]
+
+    let messagesData = [
+        {message: "Привет, семья! Как ваши дела сегодня?", id: 1},
+        {message: "Здравствуй, дорогой! Всё хорошо, занимаемся подготовкой к ужину.", id: 2},
+        {message: "Привет, всем! Я только что вернулся с тренировки, было классно.", id: 3},
+        {message: "Привет, пап! Я заканчиваю домашнее задание, скоро буду свободен.", id: 4},
+    ]
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogItems}>
 
-                <DialogsItem name={"Max"} id={1}/>
-                <DialogsItem name={"Larisa"} id={2}/>
-                <DialogsItem name={"Andrey"} id={3}/>
-                <DialogsItem name={"Bogdan"} id={4}/>
+                <DialogsItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogsItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogsItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogsItem name={dialogsData[3].name} id={dialogsData[3].id}/>
+
             </div>
 
             <div className={classes.messages}>
 
-                <Message message={"Привет, семья! Как ваши дела сегодня?"}/>
-                <Message message={"Здравствуй, дорогой! Всё хорошо, занимаемся подготовкой к ужину."}/>
-                <Message message={"Привет, всем! Я только что вернулся с тренировки, было классно."}/>
-                <Message message={"Привет, пап! Я заканчиваю домашнее задание, скоро буду свободен."}/>
+                <Message message={messagesData[0].message} id={messagesData[0].id}/>
+                <Message message={messagesData[1].message} id={messagesData[1].id}/>
+                <Message message={messagesData[2].message} id={messagesData[2].id}/>
+                <Message message={messagesData[3].message} id={messagesData[3].id}/>
             </div>
 
         </div>
