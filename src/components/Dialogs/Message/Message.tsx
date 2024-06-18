3bba7511ec06dcd0e07e90595dd9489fa9f64b4f
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-19 01:42:41 +0300

    Сщздал новую компоненту Message

---
+++ src/components/Dialogs/Message/Message.tsx
@@ -0,0 +1,11 @@
+import classes from './Dialogs.module.css'
+
+
+export type PropsMessage = {
+    message: string
+    id: number
+}
+
+export const Message = (props: PropsMessage) => {
+    return <div className={classes.message}>{props.message}</div>
+}
