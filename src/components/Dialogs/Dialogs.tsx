186a972f679fde9dae1c847f8671784a6c500acd
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-21 14:07:47 +0300

    вынес данные наружу и принял их в протипизированном виде значения props

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,23 +3,11 @@ import {DialogsItem, DialogsPropsItem} from "./DialogItem/DialogItem";
 import React from "react";
 import {Message, PropsMessage} from "./Message/Message";
 
-
-export const Dialogs = () => {
-    // Блок для отрисовки users
-    let dialogsData: DialogsPropsItem[] = [
-        {name: "Max", id: 1},
-        {name: "Larisa", id: 2},
-        {name: "Andrey", id: 3},
-        {name: "Bogdan", id: 4}
-    ]
-
-    // Блок для отрисовки Message
-    let messagesData: PropsMessage[] = [
-        {message: "Привет, семья! Как ваши дела сегодня?", id: 1},
-        {message: "Здравствуй, дорогой! Всё хорошо, занимаемся подготовкой к ужину.", id: 2},
-        {message: "Привет, всем! Я только что вернулся с тренировки, было классно.", id: 3},
-        {message: "Привет, пап! Я заканчиваю домашнее задание, скоро буду свободен.", id: 4},
-    ]
+type DialogsProps={
+    dialogsData: DialogsPropsItem[]
+    messagesData: PropsMessage[]
+}
+export const Dialogs = ({dialogsData,messagesData}:DialogsProps) => {
 
 
     const dialogsElement = dialogsData.map(dialog => {
