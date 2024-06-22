4a2ac5bf0124ab7afb863f3b7bc5d6f6b90e24cd
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-22 21:46:39 +0300

    передал объект props из state

--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -1,10 +1,6 @@
 import classes from './../Dialogs.module.css'
+import {PropsMessage} from "../../../redux/state";
 
-
-export type PropsMessage = {
-    message: string
-    id: number
-}
 
 export const Message = (props: PropsMessage) => {
     return <div className={classes.message}>{props.message}</div>
