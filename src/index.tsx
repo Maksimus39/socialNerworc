f797f199e2299ddea25e823be6d447bb7247abf0
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-23 18:05:09 +0300

    обернул App в  <BrowserRouter>

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from "./redux/state";
+import {BrowserRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <App
-            state={state}
-        />
-    </React.StrictMode>
+    <BrowserRouter>
+        <React.StrictMode>
+            <App
+                state={state}
+            />
+        </React.StrictMode>
+    </BrowserRouter>
 );
 
 reportWebVitals();
