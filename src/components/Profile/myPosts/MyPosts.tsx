5d19ff28ba0e0d03c06bf4bc5db422a2990e3568
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-23 18:05:55 +0300

    Добавил функционал для кнопки и инпут.

--- src/components/Profile/myPosts/MyPosts.tsx
+++ src/components/Profile/myPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import classes from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 import {ProfilePageType} from "../../../redux/state";
@@ -6,20 +6,27 @@ import {ProfilePageType} from "../../../redux/state";
 
 export const MyPosts = ({posts}: ProfilePageType) => {
 
-
     const postsElements = posts.map(post => {
         return (
             <Post key={post.id} message={post.message} LikeCounts={post.LikeCounts}/>
         )
     })
 
+
+    const newPostElement = useRef<HTMLTextAreaElement>(null);
+
+    const addPost = () => {
+        let text: string | undefined = newPostElement.current?.value;
+    }
+
+
     return (
 
         <div>
             <h3>My Posts</h3>
             <div className={classes.inputGroup}>
-                <textarea className={classes.postInput}></textarea>
-                <button className={classes.addPostBtn}>Add post</button>
+                <textarea className={classes.postInput} placeholder='ADD TEXT' ref={newPostElement}></textarea>
+                <button className={classes.addPostBtn} onClick={addPost}>Add post</button>
             </div>
             <div className={classes.posts}>
 
