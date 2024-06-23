c7cd165310b8c82a50b6c4b95d5e107061b0525f
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-23 18:02:12 +0300

    Вынес  <BrowserRouter> из компоненты.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navdar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
@@ -17,31 +17,27 @@ export type AppProps = {
 
 function App({state}: AppProps) {
 
-
     return (
-        <BrowserRouter>
 
-            <div className='app-wrapper'>
+        <div className='app-wrapper'>
 
-                <Header/>
-                <Navbar/>
+            <Header/>
+            <Navbar/>
 
-                <div className='app-wrapper-content'>
+            <div className='app-wrapper-content'>
 
-                    <Routes>
+                <Routes>
 
-                        <Route path='/profile' element={<Profile posts={state.profilePage.posts}/>}/>
-                        <Route path='/dialogs'
-                               element={<Dialogs dialogs={state.profilePage.dialogs} messages={state.dialogsPage.messages}/>}/>
-                        <Route path='/news' element={<News/>}/>
-                        <Route path='/music' element={<Music/>}/>
-                        <Route path='/settings' element={<Settings/>}/>
-
-                    </Routes>
-
-                </div>
+                    <Route path='/profile' element={<Profile posts={state.profilePage.posts}/>}/>
+                    <Route path='/dialogs'
+                           element={<Dialogs dialogs={state.profilePage.dialogs}
+                                             messages={state.dialogsPage.messages}/>}/>
+                    <Route path='/news' element={<News/>}/>
+                    <Route path='/music' element={<Music/>}/>
+                    <Route path='/settings' element={<Settings/>}/>
+                </Routes>
             </div>
-        </BrowserRouter>
+        </div>
     )
 }
 
