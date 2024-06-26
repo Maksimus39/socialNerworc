00cc87d9c7450f5f24d3e659ff2085a0b4638fc3
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-26 12:59:37 +0300

    Обратный перенос функции rerenderEntereTree

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,33 @@
 import './index.css';
-import {rerenderEntereTree} from "./render";
-import state from "./redux/state";
+import state, {addPost, RootStateType, subscribe, updateNewPostText} from "./redux/state";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+
+// Создаем корень один раз, в глобальной области видимости модуля
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const rerenderEntereTree = (state:RootStateType) => {
+    root.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App
+                    state={state}
+                    addPost={addPost}
+                    updateNewPostText={updateNewPostText}
+                />
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+}
+
+// Теперь функция rerenderEntereTree использует уже созданный корень, когда ее вызывают
+
+reportWebVitals();
 
 
 rerenderEntereTree(state)
 
+subscribe(()=>rerenderEntereTree(state))
